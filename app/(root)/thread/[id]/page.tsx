14f56a51f89f6d9632ba7cc4caa88e73e475666a
Dan <daniel.speranskiy@live.com>
2023-11-02 09:44:05 +0400

    Displaying strings in home section via creation

---
+++ app/(root)/thread/[id]/page.tsx
@@ -0,0 +1,13 @@
+
+
+const Page = ({params}: {params: {id: string}}) =>{
+    <section className="relative">
+        <div>
+            
+        </div>
+    </section>
+}
+
+
+export default Page
+
